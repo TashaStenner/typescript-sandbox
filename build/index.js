868e3f56e20d8004e1dc8b2094694bbda0488701
Natasha Stenner <tashastenner@natashas-mbp.home>
2020-09-15 16:20:21 +0100

    interfaces

--- build/index.js
+++ build/index.js
@@ -1,10 +1,53 @@
 const greet = (firstName, lastName) => {
     return firstName + " " + lastName;
 };
-console.log(greet("natasha", "stenner"));
+console.log(greet("Natasha", "Stenner"));
 const calculateAgeDifference = (age1, age2) => {
     let ageDifference = age1 - age2;
     return ("The age difference is " + ageDifference + " years");
 };
 console.log(calculateAgeDifference(32, 23));
+const fixColours = (colorArray, index) => {
+    let checkedColor = colorArray[index];
+    let newArray = [...colorArray];
+    if (checkedColor != "red" && checkedColor != "blue" && checkedColor != "yellow") {
+        newArray.splice(index, 1, "red");
+        return newArray;
+    }
+    return colorArray;
+};
+console.log(fixColours(["yellow", "blue", "red", "red"], 1));
+console.log(fixColours(["yellow", "purple", "red", "red"], 2));
+console.log(fixColours(["yellow", "purple", "green", "red"], 1));
+console.log(fixColours(["yellow", "blue", "red", "red"], 0));
+const tasha = {
+    name: "Tasha",
+    age: 22,
+    email: "tashas1997@gmail.com",
+    phone: {
+        model: "Samsung",
+        telNumber: 123456,
+        hasInsurance: true,
+    }
+};
+const graham = {
+    name: "Graham",
+    age: 31,
+    email: "graham@graham.com",
+    phone: {
+        model: "Iphone",
+        telNumber: 1233456,
+        hasInsurance: false,
+    }
+};
+const katie = {
+    name: "katie",
+    age: 21,
+    phone: {
+        model: "Iphone",
+        telNumber: 123345996,
+        hasInsurance: true,
+    }
+};
+const people = [tasha, graham, katie];
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -2,12 +2,73 @@ const greet = (firstName: string, lastName: string): string => {
     return firstName + " " + lastName
 }
 
-console.log(greet("natasha", "stenner"))
+console.log(greet("Natasha", "Stenner"))
 
-const calculateAgeDifference = (age1: number, age2: number):string => {
+const calculateAgeDifference = (age1: number, age2: number): string => {
     let ageDifference = age1 - age2;
     return ("The age difference is " + ageDifference + " years")
 }
 
-console.log(calculateAgeDifference(32, 23))  
+console.log(calculateAgeDifference(32, 23))
 
+const fixColours = (colorArray: string[], index: number): string[] => {
+    let checkedColor: string = colorArray[index];
+    let newArray: string[] = [...colorArray];
+    if (checkedColor != "red" && checkedColor != "blue" && checkedColor != "yellow") {
+        newArray.splice(index, 1, "red");
+        return newArray;
+    }
+    return colorArray;
+};
+console.log(fixColours(["yellow", "blue", "red", "red"], 1));
+console.log(fixColours(["yellow", "purple", "red", "red"], 2));
+console.log(fixColours(["yellow", "purple", "green", "red"], 1));
+console.log(fixColours(["yellow", "blue", "red", "red"], 0));
+
+
+interface IPerson {
+    name: string;
+    age: number;
+    email?: string;
+    phone?: IPhone;
+}
+
+interface IPhone {
+    model: string;
+    telNumber: number;
+    hasInsurance: boolean;
+}
+
+const tasha: IPerson = {
+    name: "Tasha",
+    age: 22,
+    email: "tashas1997@gmail.com",
+    phone: {
+        model: "Samsung",
+        telNumber: 123456,
+        hasInsurance: true,
+    }
+}
+
+const graham: IPerson = {
+    name: "Graham",
+    age: 31,
+    email: "graham@graham.com",
+    phone: {
+        model: "Iphone",
+        telNumber: 1233456,
+        hasInsurance: false,
+    }
+}
+
+const katie: IPerson = {
+    name: "katie",
+    age: 21,
+    phone: {
+        model: "Iphone",
+        telNumber: 123345996,
+        hasInsurance: true,
+    }
+}
+
+const people: IPerson[] = [tasha, graham, katie]; 
